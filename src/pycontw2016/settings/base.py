905313b802bc5cbc82d2e1a5764873203f4b5c30
Andy Dai <daikeren@gmail.com>
2015-11-15 00:24:47 +0800

    Added request context processor

--- src/pycontw2016/settings/base.py
+++ src/pycontw2016/settings/base.py
@@ -33,6 +33,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
             'debug': True,
         },
