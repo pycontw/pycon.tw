cfb21cbcdfbb1d5b1e33bc88e43ef3088380af94
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-01 18:19:37 +0800

    Use email instead of name in __str__

--- src/users/models.py
+++ src/users/models.py
@@ -118,7 +118,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         swappable = 'AUTH_USER_MODEL'
 
     def __str__(self):
-        return self.speaker_name
+        return self.email
 
     def get_full_name(self):
         return self.speaker_name
