7a458f179e5c421bc77f7e1bfc5890e3e20a313f
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-04 15:53:52 +0800

    Model fields should use lazy translation

--- src/proposals/models.py
+++ src/proposals/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class Proposal(models.Model):
