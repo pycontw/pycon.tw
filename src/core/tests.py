7264a560a81d3f099f698370447c055890b70577
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-22 22:40:08 +0800

    Implement catch-all template view

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,4 +1,17 @@
+import pytest
+
+
 def test_index_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert 'PyCon' in response.content.decode('utf-8')
+
+
+@pytest.mark.parametrize('path,expected', [
+    ('/speaking/cfp/',   200),
+    ('/speaking/talk/',  200),
+    ('/speaking/base/',  404),
+    ('/speaking/_base/', 404),
+])
+def test_speaking_pages(client, path, expected):
+    assert client.get(path).status_code == expected
