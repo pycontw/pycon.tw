59fc100f7740f1dc1861109d6b57c32758e15c4f
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-23 17:32:03 +0800

    Supply some additional proposal form tests

--- src/proposals/tests/test_forms.py
+++ src/proposals/tests/test_forms.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import AnonymousUser
+
 from proposals.forms import (
     TalkProposalCreateForm, TalkProposalUpdateForm,
     TutorialProposalCreateForm, TutorialProposalUpdateForm,
@@ -12,12 +14,144 @@ def test_talk_proposal_create_form():
     ]
 
 
+def test_talk_proposal_create_form_no_request():
+    form = TalkProposalCreateForm(data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'PREF45',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': ['Proposal creation requires a request object.'],
+    }
+
+
+def test_talk_proposal_create_form_anonymous(request):
+    request.user = AnonymousUser()
+    form = TalkProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'PREF45',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': [
+            'Only authenticated user with complete speaker profile may '
+            'submit a proposal.',
+        ],
+    }
+
+
+def test_talk_proposal_create_form_no_profile(request, bare_user):
+    request.user = bare_user
+    form = TalkProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'PREF45',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': [
+            'Only authenticated user with complete speaker profile may '
+            'submit a proposal.',
+        ],
+    }
+
+
+def test_talk_proposal_create_form_valid(request, user):
+    request.user = user
+    form = TalkProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'PREF45',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert form.is_valid()
+
+
 def test_tutorial_proposal_create_form():
     form = TutorialProposalCreateForm()
     assert list(form.fields) == [
         'title', 'category', 'duration', 'language',
         'python_level', 'recording_policy',
     ]
+
+
+def test_tutorial_proposal_create_form_no_request():
+    form = TutorialProposalCreateForm(data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'HALFDAY',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': ['Proposal creation requires a request object.'],
+    }
+
+
+def test_tutorial_proposal_create_form_anonymous(request):
+    request.user = AnonymousUser()
+    form = TutorialProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'HALFDAY',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': [
+            'Only authenticated user with complete speaker profile may '
+            'submit a proposal.',
+        ],
+    }
+
+
+def test_tutorial_proposal_create_form_no_profile(request, bare_user):
+    request.user = bare_user
+    form = TutorialProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'HALFDAY',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert not form.is_valid()
+    assert form.errors == {
+        '__all__': [
+            'Only authenticated user with complete speaker profile may '
+            'submit a proposal.',
+        ],
+    }
+
+
+def test_tutorial_proposal_create_form_valid(request, user):
+    request.user = user
+    form = TutorialProposalCreateForm(request=request, data={
+        'title': 'Beyond the Style Guides',
+        'category': 'PRAC',
+        'duration': 'HALFDAY',
+        'language': 'CHI',
+        'python_level': 'INTERMEDIATE',
+        'recording_policy': True,
+    })
+    assert form.is_valid()
 
 
 def test_talk_proposal_update_form():
