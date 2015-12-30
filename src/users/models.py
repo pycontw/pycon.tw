07e83e64635e7b92c664dd128fb832f0bb4622fa
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-30 23:38:31 +0800

    Setup flake8 and cleanup things

--- src/users/models.py
+++ src/users/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin,
 )
-from django.contrib.contenttypes.models import ContentType
 from django.core import signing
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
@@ -12,8 +11,6 @@ from django.db import models
 from django.template.loader import render_to_string
 from django.utils import timezone
 from django.utils.translation import ugettext, ugettext_lazy as _
-
-from proposals.models import TalkProposal, TutorialProposal
 
 
 class UserQueryset(models.QuerySet):
