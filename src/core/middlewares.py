80cf034928aba9f67d73f24879326f92f97e37da
Tzu-ping Chung <uranusjr@gmail.com>
2015-12-31 16:13:52 +0800

    Relax language fallback to allow things like "/en"

--- src/core/middlewares.py
+++ src/core/middlewares.py
@@ -4,9 +4,16 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 
 
+# Matches things like
+#   /en
+#   /en/
+#   /en/foo/bar (can be anything after the first trailing slash)
+# But not
+#   /en-gb
+# because the fallback language code is not followed immediately by a slash.
 FALLBACK_PREFIX_PATTERN = re.compile(
-    r'^/(?P<lang>{langs})/'.format(
-        langs='|'.join(settings.FALLBACK_LANGUAGE_PREFIXES),
+    r'^/(?P<lang>{langs})(?:/?|/.+)$'.format(
+        langs='|'.join(settings.FALLBACK_LANGUAGE_PREFIXES.keys()),
     ),
     re.UNICODE,
 )
