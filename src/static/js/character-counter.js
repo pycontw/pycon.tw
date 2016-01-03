760fc00dae1a8195268082b524e6c2f523b47dc3
Tzu-ping Chung <uranusjr@gmail.com>
2016-01-04 00:30:13 +0800

    Add simple character counter to biography field

---
+++ src/static/js/character-counter.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+var showCharacterCount = function ($counter, $source) {
+	var text = $source.val();
+	var length = text ? text.length : 0;
+	$counter.text(length);
+	$counter.closest('.form-group').toggleClass(
+		'has-error', length > parseInt($counter.data('limit')));
+};
+
+$('.character-counter .character-count-display').each(function () {
+	var $counter = $(this);
+	$source = $('textarea#' + $counter.data('source-id'));
+	$source.on('input propertychange', function () {
+			showCharacterCount($counter, $(this));
+		});
+	showCharacterCount($counter, $source);
+});
+
+})(jQuery);
