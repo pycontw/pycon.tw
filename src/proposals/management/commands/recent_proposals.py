dc1497fea98743e7db1f8f03450332b797c35890
Liang Bo Wang <ccwang002@gmail.com>
2016-01-19 16:04:08 +0800

    Fix accidentally miss proposals as daily cron job
    Now the behavior has changed, so has the command options. Summary of
    all changes:
    
    - All operations are timezone aware, and hard-coded to use Taiwan
      timezone.
    - Add an hour option which should match the hour when the cron job runs.
    
    The detail about the behavior change is that daily cron jobs guarantee
    to run after the specified time, say at 21:00 everyday, but it won't be
    the exact same time. Previously, we compute the exact 24 x N hours to
    find the proposal, so we might lose some talks submitted around 21:00.
    
    For example, if the cron job is set to run at 21:00, with command
    options being `--days 3 --hour 21`. An example of the running schedule
    can be
    
        Nth Job | Actual run time | Report proposals submitted during
        1st     | Jan 10 21:15    | Jan  8 21:00 - Jan 10 20:59
        2nd     | Jan 13 21:34    | Jan 10 21:00 - Jan 13 21:00
    
    If the same command is called on 20:30, it failed with a CommandError
    because 21:00 has yet to come.
    
    Surprise hidden in the comment: a set of unittest based on test time has
    been created. Welcome to the stochastic and unpredictable failing tests
    :)

--- src/proposals/management/commands/recent_proposals.py
+++ src/proposals/management/commands/recent_proposals.py
@@ -1,22 +1,37 @@
 import logging
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from tabulate import tabulate
+import pytz
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
-from django.utils.timezone import now
 
 from proposals.models import TalkProposal, TutorialProposal
 
 logger = logging.getLogger(__file__)
 
 
-def str_stripper(s, max_len=32):
+def str_stripper(s, max_len=32, ellipsis='...'):
     if len(s) > max_len:
-        return s[:max_len - len('...')] + '...'
+        return s[:max_len - len(ellipsis)] + ellipsis
     else:
         return s
+
+
+def proposal_summary(queryset):
+    table = []
+    for p in queryset:
+        table.append((
+            str_stripper(p.get_category_display(), 16),
+            str_stripper(p.get_python_level_display(), 16),
+            p.title
+        ))
+    return tabulate(
+        table,
+        ['Category', 'Python Level', 'Title'],
+        tablefmt='simple'
+    )
 
 
 class Command(BaseCommand):
@@ -24,26 +39,26 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--recent',
+            '--days',
             metavar='N',
             type=int,
             default=3,
             help='Collect proposals submitted in the latest N days.'
         )
+        parser.add_argument(
+            '--hour',
+            metavar='H',
+            type=int,
+            default=21,
+            help="""
+            Set the hour to cut the day shift. For example, if 21 O\'clock in
+            is set, it finds proposals submitted between today-N 21:00 to today
+            21:00. An error will be raised if the given hour time has not come.
+            """
+        )
 
     def handle(self, *args, **options):
-        recent_days = options['recent']
-        if recent_days <= 0:
-            raise CommandError(
-                'Provide positive number of days, getting %d'
-                % recent_days
-            )
-        self.stderr.write(
-            'Collecting recent {:d} days proposals ...'
-            .format(recent_days)
-        )
-        earliest_dt = now() - timedelta(days=recent_days)
-        recent_lookup = Q(created_at__gte=earliest_dt) & Q(cancelled=False)
+        recent_lookup = self.create_datetime_range_lookup(*args, **options)
         recent_talks = TalkProposal.objects.filter(recent_lookup)
         recent_tutorials = TutorialProposal.objects.filter(recent_lookup)
         if not recent_talks.exists() and not recent_tutorials.exists():
@@ -57,11 +72,51 @@ class Command(BaseCommand):
 
         if recent_talks:
             self.stdout.write('\n\nTalks:')
-            self.proposal_summary(recent_talks)
+            self.stdout.write(proposal_summary(recent_talks))
 
         if recent_tutorials:
             self.stdout.write('\n\nTutorials:')
-            self.proposal_summary(recent_tutorials)
+            self.stdout.write(proposal_summary(recent_tutorials))
+
+    def create_datetime_range_lookup(self, *args, **options):
+        """Create valid recent datetime range and return a lookup Q object"""
+        utc_tz = pytz.UTC
+        taiwan_tz = pytz.timezone('Asia/Taipei')
+        recent_days = options['days']
+        if recent_days <= 0:
+            raise CommandError(
+                'Given number of days %d is not a positive number'
+                % recent_days
+            )
+        day_shift_hour = options['hour']
+        today_utc_dt = utc_tz.fromutc(datetime.utcnow())
+        # To find the datetime range in Taiwan timezone
+        #   today-N H:00 to today H:00
+        try:
+            today_dt = taiwan_tz.normalize(today_utc_dt).replace(
+                hour=day_shift_hour, minute=0, second=0, microsecond=0
+            )
+        except ValueError as e:
+            raise CommandError(
+                'Given hour %d is invalid' % day_shift_hour
+            ) from e
+        if today_dt > today_utc_dt:
+            raise CommandError(
+                "Today's datetime {:%Y-%m-%d %H:%M} ({!s}) is yet present"
+                .format(today_dt, taiwan_tz)
+            )
+        earliest_dt = today_dt - timedelta(days=recent_days)
+        self.stderr.write(
+            'Collecting recent {:d} days proposals '
+            'from {:%Y-%m-%d %H:%M} to {:%Y-%m-%d %H:%M} (timezone: {!s})'
+            .format(recent_days, earliest_dt, today_dt, taiwan_tz)
+        )
+        recent_lookup = Q(
+            created_at__gte=earliest_dt,
+            created_at__lt=today_dt,
+            cancelled=False,
+        )
+        return recent_lookup
 
     def cry(self):
         self.stderr.write(self.style.NOTICE(
@@ -69,16 +124,4 @@ class Command(BaseCommand):
             '◢▆▅▄▃ 崩╰(〒皿〒)╯潰 ▃▄▅▆◣'
         ))
 
-    def proposal_summary(self, queryset):
-        table = []
-        for p in queryset:
-            table.append((
-                str_stripper(p.get_category_display(), 16),
-                str_stripper(p.get_python_level_display(), 16),
-                p.title
-            ))
-        self.stdout.write(tabulate(
-            table,
-            ['Category', 'Python Level', 'Title'],
-            tablefmt='simple'
-        ))
+
--- src/proposals/tests/test_management.py
+++ src/proposals/tests/test_management.py
@@ -2,23 +2,62 @@ from datetime import timedelta
 import re
 
 import pytest
+import pytz
 from django.utils.timezone import now
 from django.core.management import call_command
 from django.core.management.base import CommandError
 
 from proposals.models import TalkProposal
 
+taiwan_tz = pytz.timezone('Asia/Taipei')
+
+
+# Define fixtures
+
+@pytest.fixture()
+def today_valid_hour():
+    """The valid hour that has been presented today"""
+    return taiwan_tz.normalize(now()).hour
+
+
+def make_proposal_created_earlier(proposal, days=1):
+    dt_dayago = now() - timedelta(days=days)
+    proposal.created_at = dt_dayago
+    proposal.save()
+
+
+@pytest.fixture()
+def dayago_talk_proposal(user):
+    proposal = TalkProposal.objects.create(
+        id=77,
+        submitter=user,
+        title='How to build a time machine in 1 day',
+        category='SCI'
+    )
+    make_proposal_created_earlier(proposal)
+    return proposal
+
+
+@pytest.fixture()
+def another_user_dayago_talk_proposal(another_user):
+    proposal = TalkProposal.objects.create(
+        id=9527,
+        submitter=another_user,
+        title='Transition from Ruby to Python',
+        category='CORE',
+    )
+    make_proposal_created_earlier(proposal)
+    return proposal
+
 
 @pytest.fixture()
 def weekago_talk_proposal(user):
-    dt_weekago = now() - timedelta(weeks=1)
     proposal = TalkProposal.objects.create(
         id=56,
         submitter=user,
         title='Long long time ago when Python was still 2.x',
     )
-    proposal.created_at = dt_weekago
-    proposal.save()
+    make_proposal_created_earlier(proposal, days=7)
     return proposal
 
 
@@ -28,32 +67,19 @@ def test_weekago_talk_created_datetime(weekago_talk_proposal):
     assert proposal_lifetime >= timedelta(weeks=1)
 
 
-@pytest.fixture()
-def another_user_dayago_talk_proposal(another_user):
-    dt_dayago = now() - timedelta(days=1)
-    proposal = TalkProposal.objects.create(
-        id=9527,
-        submitter=another_user,
-        title='Transition from Ruby to Python',
-        category='CORE',
-        created_at=dt_dayago,
-    )
-    return proposal
-
-
 def test_recent_proposal_default_command(
-    talk_proposal, weekago_talk_proposal,
+    dayago_talk_proposal, weekago_talk_proposal,
     another_user_dayago_talk_proposal,
-    capsys,
+    today_valid_hour, capsys,
 ):
-    call_command('recent_proposals')
+    call_command('recent_proposals', hour=today_valid_hour)
     out, err = capsys.readouterr()
     print(out)
 
     # Test only two talk proposals are retrieved
     assert re.search(r"Got total 2 new proposals", out, re.MULTILINE)
     # Test the title of these two proposals are in the output
-    for proposal in [talk_proposal, another_user_dayago_talk_proposal]:
+    for proposal in [dayago_talk_proposal, another_user_dayago_talk_proposal]:
         assert re.search(proposal.title, out, re.MULTILINE)
 
     # Test the title of outdated proposals are not in the output
@@ -64,9 +90,9 @@ def test_cancelled_proposal_not_shown_in_recent_proposals(
     cancelled_talk_proposal,
     another_user_dayago_talk_proposal,
     weekago_talk_proposal,
-    capsys,
+    today_valid_hour, capsys,
 ):
-    call_command('recent_proposals', recent=6)
+    call_command('recent_proposals', days=6, hour=today_valid_hour)
     out, err = capsys.readouterr()
 
     # Test only one talk proposal is retrieved
@@ -78,8 +104,12 @@ def test_cancelled_proposal_not_shown_in_recent_proposals(
         )
 
 
-def test_recent_tutorial_proposals_only(tutorial_proposal, capsys):
-    call_command('recent_proposals')
+def test_recent_tutorial_proposals_only(
+    tutorial_proposal,
+    today_valid_hour, capsys
+):
+    make_proposal_created_earlier(tutorial_proposal)
+    call_command('recent_proposals', hour=today_valid_hour)
     out, err = capsys.readouterr()
 
     assert 'Talks:\n' not in out
@@ -87,24 +117,51 @@ def test_recent_tutorial_proposals_only(tutorial_proposal, capsys):
     assert re.search(r"^Got total 1 new proposals", out, re.MULTILINE)
 
 
+# Testing edge cases
+
 @pytest.mark.django_db
-@pytest.mark.parametrize('recent_days', [-1, 0])
-def test_nonpositive_recent_days(recent_days):
+@pytest.mark.parametrize('days', [-1, 0])
+def test_nonpositive_recent_days(days, today_valid_hour):
     with pytest.raises(CommandError) as e:
-        call_command('recent_proposals', recent=recent_days)
-    assert 'Provide positive number' in str(e.value)
-
+        call_command('recent_proposals', days=days, hour=today_valid_hour)
+    assert 'not a positive number' in str(e.value)
 
 
 @pytest.mark.django_db
-def test_no_recent_proposal(capsys):
-    call_command('recent_proposals')
+def test_no_recent_proposal(today_valid_hour, capsys):
+    call_command('recent_proposals', hour=today_valid_hour)
     out, err = capsys.readouterr()
     print(err)
     assert re.search('^No proposals are recently submitted', err, re.MULTILINE)
 
 
-def test_output_table_trimming(another_user_dayago_talk_proposal, capsys):
-    call_command('recent_proposals')
+def test_justly_created_proposal(talk_proposal, today_valid_hour, capsys):
+    call_command('recent_proposals', hour=today_valid_hour)
+    out, err = capsys.readouterr()
+    assert re.search('^No proposals are recently submitted', err, re.MULTILINE)
+
+
+@pytest.mark.parametrize('hour', [-1, 25])
+def test_invalid_hour(hour):
+    with pytest.raises(CommandError) as e:
+        call_command('recent_proposals', hour=hour)
+    assert 'Given hour %d is invalid' % hour in str(e.value)
+
+
+def test_yet_present_hour():
+    now_dt = taiwan_tz.normalize(now())
+    if now_dt == 23:
+        # FIXME: cannot test this between 23:00 - 23:59
+        return
+    with pytest.raises(CommandError) as e:
+        call_command('recent_proposals', hour=now_dt.hour + 1)
+    assert 'yet present' in str(e.value)
+
+
+def test_output_table_trimming(
+    another_user_dayago_talk_proposal,
+    today_valid_hour, capsys
+):
+    call_command('recent_proposals', hour=today_valid_hour)
     out, err = capsys.readouterr()
     assert re.search(r'^Python Core \(...\s+', out, re.MULTILINE)
