be92b5ace86738f3d9572fb63615969537b787ab
Michelle <flywindy2002@gmail.com>
2016-03-16 12:05:07 +0800

    Add get_ordering

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -16,13 +16,21 @@ class TalkProposalListView(PermissionRequiredMixin, ListView):
     model = TalkProposal
     permission_required = REVIEW_REQUIRED_PERMISSIONS
     template_name = 'reviews/talk_proposal_list.html'
+    ordering = '?'
     order_keys = {
         'title': 'title',
         'reviews': 'review_count',
+        'category': 'category'
     }
 
-    def get_queryset(self):
+    def get_ordering(self):
         params = self.request.GET
+        order_key = self.order_keys.get(params.get('order', '').lower())
+        if order_key:
+            self.ordering = order_key
+        return self.ordering
+
+    def get_queryset(self):
         user = self.request.user
         proposals = (
             self.model.objects
@@ -30,11 +38,11 @@ class TalkProposalListView(PermissionRequiredMixin, ListView):
             .exclude(review__stage=ReviewsConfig.stage, review__reviewer=user)
             .annotate(review_count=Count('review'))
         )
-        category = params.get('category', '').upper()
-        if category:
-            proposals = proposals.filter(category=category)
-        order_key = self.order_keys.get(params.get('order', '').lower())
-        return proposals.order_by(order_key or '?')
+        # params = self.request.GET
+        # category = params.get('category', '').upper()
+        # if category:
+        #     proposals = proposals.filter(category=category)
+        return proposals.order_by(self.get_ordering())
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -53,6 +61,7 @@ class TalkProposalListView(PermissionRequiredMixin, ListView):
             'strong_reject': self.get_reviews().filter(score=-2).count(),
         }
 
+        context['ordering'] = self.ordering
         return context
 
     def get_reviews(self):
