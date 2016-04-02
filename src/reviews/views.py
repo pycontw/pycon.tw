61f6e24fcda19052fb9b5a51ff8d8794d1a4b71e
Tzu-ping Chung <uranusjr@gmail.com>
2016-04-02 15:46:23 +0800

    Show only proposals w/o verdict in review list tab

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -64,7 +64,9 @@ class TalkProposalListView(PermissionRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['reviews'] = self.get_reviews()
+        context['reviews'] = self.get_reviews().filter(
+            proposal__accepted__isnull=True,
+        )
 
         verdicted_proposals = (
             TalkProposal.objects
