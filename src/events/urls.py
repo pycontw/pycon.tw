feb332ca76c9d07b76c4e55383a7178b0efd204f
Tzu-ping Chung <uranusjr@gmail.com>
2016-04-26 20:31:14 +0800

    Show sponsored events in view

--- src/events/urls.py
+++ src/events/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^talks/$', views.TalkListView.as_view(), name='events_talk_list'),
     url(r'^talk/(?P<pk>\d+)/$', views.TalkDetailView.as_view(),
         name='events_talk_detail'),
+    url(r'^sponsored/(?P<pk>\d+)/$', views.SponsoredEventDetailView.as_view(),
+        name='events_sponsored_event_detail'),
 ]
--- src/events/views.py
+++ src/events/views.py
@@ -2,6 +2,8 @@ from django.views.generic import DetailView, ListView
 
 from proposals.models import TalkProposal
 
+from .models import SponsoredEvent
+
 
 class AcceptedTalkMixin:
     queryset = (
@@ -12,8 +14,25 @@ class AcceptedTalkMixin:
 
 
 class TalkListView(AcceptedTalkMixin, ListView):
+
     template_name = 'events/talk_list.html'
+
+    def get_context_data(self, **kwargs):
+        sponsored_events = (
+            SponsoredEvent.objects
+            .select_related('host')
+            .order_by('title')
+        )
+        return super().get_context_data(
+            sponsored_events=sponsored_events,
+            **kwargs
+        )
 
 
 class TalkDetailView(AcceptedTalkMixin, DetailView):
     template_name = 'events/talk_detail.html'
+
+
+class SponsoredEventDetailView(DetailView):
+    model = SponsoredEvent
+    template_name = 'events/sponsored_event_detail.html'
