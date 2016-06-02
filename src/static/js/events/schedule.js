6e65051f575894d38507b10643a5e4d5b7b69cdc
Tzu-ping Chung <uranusjr@gmail.com>
2016-06-02 22:44:29 +0800

    Update hash on schedule page on scroll
    This makes it easier to share the schedule on a specific day.

--- src/static/js/events/schedule.js
+++ src/static/js/events/schedule.js
@@ -6,4 +6,23 @@ Hyphenator.run();
 
 $('.talk__title').dotdotdot({wrap: 'word', watch: true, fallbackToLetter: true});
 
+// Scrollspy-like behavior to update the hash on scroll.
+if (window.history && window.history.replaceState) {
+	$(document).on('scroll', function () {
+		var $document = $(this);
+		var ids = ['2016-06-05', '2016-06-04', '2016-06-03'];
+		var hash = '';
+		$.each(ids, function () {
+			if ($document.scrollTop() >= $('#' + this).offset().top - 1) {
+				hash = '#' + this;
+				return false;
+			}
+		});
+		if (hash !== window.location.hash) {
+			var nurl = window.location.pathname + window.location.search;
+			window.history.replaceState('', '', nurl + hash);
+		}
+	});
+}
+
 })(jQuery);
