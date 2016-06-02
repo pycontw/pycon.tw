cb30b602c2e1c48b943f82352a2cb337a5a98af2
starwilly <willystar@gmail.com>
2016-06-02 22:20:52 +0800

    update text truncate when window's size change

--- src/static/js/events/schedule.js
+++ src/static/js/events/schedule.js
@@ -4,6 +4,6 @@ $('.talk__title').addClass('hyphenate');
 Hyphenator.config({useCSS3hyphenation: true});
 Hyphenator.run();
 
-$('.talk__title').dotdotdot({'wrap': 'letter'});
+$('.talk__title').dotdotdot({wrap: 'word', watch: true, fallbackToLetter: true});
 
 })(jQuery);
