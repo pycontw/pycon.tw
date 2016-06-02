28ab9fb94cd3e9761644df2e905080161db56fa6
Tzu-ping Chung <uranusjr@gmail.com>
2016-06-02 22:46:36 +0800

    Disable hyphenator for now

--- src/static/js/events/schedule.js
+++ src/static/js/events/schedule.js
@@ -1,8 +1,8 @@
 (function ($) {
 
-$('.talk__title').addClass('hyphenate');
-Hyphenator.config({useCSS3hyphenation: true});
-Hyphenator.run();
+// $('.talk__title').addClass('hyphenate');
+// Hyphenator.config({useCSS3hyphenation: true});
+// Hyphenator.run();
 
 $('.talk__title').dotdotdot({wrap: 'word', watch: true, fallbackToLetter: true});
 
