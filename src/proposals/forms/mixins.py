f50d88614dd55d3483954d6c01c2ad9467787893
Tzu-ping Chung <uranusjr@gmail.com>
2017-02-10 16:32:42 +0800

    Move help text out of proposal fields
    This makes abstraction of talk and tutorial proposal models easier, and
    prevent migrations from happening when we tweak the help texts.

--- src/proposals/forms/mixins.py
+++ src/proposals/forms/mixins.py
@@ -2,6 +2,8 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from core.forms import RequestUserValidationMixin
+from core.widgets import CharacterCountedTextarea, SimpleMDEWidget
+from proposals.models import TalkProposal, TutorialProposal
 
 
 class RequestUserSpeakerValidationMixin(RequestUserValidationMixin):
@@ -21,3 +23,91 @@ class RequestUserSpeakerValidationMixin(RequestUserValidationMixin):
         if user.is_anonymous() or not user.is_valid_speaker():
             raise forms.ValidationError(self.get_error_message('bad_speaker'))
         return self.cleaned_data
+
+
+class TalkProposalMixin:
+    """Provide metadata for talk proposal forms.
+    """
+    class Meta:
+        model = TalkProposal
+        widgets = {
+            'abstract': CharacterCountedTextarea(),
+            'objective': CharacterCountedTextarea(),
+            'detailed_description': SimpleMDEWidget(),
+            'outline': SimpleMDEWidget(),
+            'supplementary': SimpleMDEWidget(),
+        }
+        help_texts = {
+            'objective': _(
+                "<p><a href='#' data-toggle='modal' "
+                "data-target='#proposalFieldExampleModal' "
+                "data-content='objective'>Proposal Examples</a></p>"
+                "Who is the intended audience for your talk? (Be specific, "
+                "\"Python users\" is not a good answer). "
+                "And what will the attendees get out of your talk? When they "
+                "leave the room, what will they learn that they didn't know "
+                "before? This is NOT made public and for REVIEW ONLY."
+            ),
+            'supplementary': _(
+                "Anything else you'd like the program committee to know when "
+                "making their selection: your past speaking experience, "
+                "community experience, etc. This is NOT made public and for "
+                "REVIEW ONLY. Edit using "
+                "<a href='http://daringfireball.net/projects/markdown/basics' "
+                "target='_blank' rel='noopener'>Markdown</a>."
+            ),
+            'outline': _(
+                "<p><a href='#' data-toggle='modal' "
+                "data-target='#proposalFieldExampleModal' "
+                "data-content='outline'>Proposal Examples</a></p>"
+                "How the talk will be arranged. It is highly recommended to "
+                "attach the estimated time length for each sections in the "
+                "talk. Talks in favor of 45min should have a fallback plan "
+                "about how to shrink the content into a 30min one. Edit using "
+                "<a href='http://daringfireball.net/projects/markdown/basics' "
+                "target='_blank' rel='noopener'>Markdown</a>. "
+                "This is NOT made public and for REVIEW ONLY."
+            ),
+        }
+
+
+class TutorialProposalMixin:
+    """Provide metadata for tutorial proposal forms.
+    """
+    class Meta:
+        model = TutorialProposal
+        widgets = {
+            'abstract': CharacterCountedTextarea(),
+            'objective': CharacterCountedTextarea(),
+            'detailed_description': SimpleMDEWidget(),
+            'outline': SimpleMDEWidget(),
+            'supplementary': SimpleMDEWidget(),
+        }
+        help_texts = {
+            'objective': _(
+                "<p><a href='#' data-toggle='modal' "
+                "data-target='#proposalFieldExampleModal' "
+                "data-content='objective'>Proposal Examples</a></p>"
+                "Who is the intended audience for your talk? (Be specific, "
+                "\"Python users\" is not a good answer). "
+                "And what will the attendees get out of your talk? When they "
+                "leave the room, what will they learn that they didn't know "
+                "before? This is NOT made public and for REVIEW ONLY."
+            ),
+            'supplementary': _(
+                "Anything else you'd like the program committee to know when "
+                "making their selection: your past speaking experience, "
+                "community experience, etc. This is NOT made public and for "
+                "REVIEW ONLY. Edit using "
+                "<a href='http://daringfireball.net/projects/markdown/basics' "
+                "target='_blank' rel='noopener'>Markdown</a>."
+            ),
+            'outline': _(
+                "How the tutorial will be arranged. You should enumerate over "
+                "each section in your talk and attach each section with the "
+                "estimated time length. Edit using "
+                "<a href='http://daringfireball.net/projects/markdown/basics' "
+                "target='_blank' rel='noopener'>Markdown</a>. "
+                "This is NOT made public and for REVIEW ONLY."
+            ),
+        }
