84fca1d461cca9c43d668e342ef258f107248bab
Tzu-ping Chung <uranusjr@gmail.com>
2017-04-02 15:34:49 +0800

    Add order by to provide distinction

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -61,7 +61,7 @@ class TalkProposalListView(ReviewableMixin, PermissionRequiredMixin, ListView):
             .exclude(accepted__isnull=False)
             .exclude(review__reviewer=user)
             .annotate(review_count=Count('review'))
-            .distinct()
+            .order_by('pk')
         )
         # params = self.request.GET
         # category = params.get('category', '').upper()
