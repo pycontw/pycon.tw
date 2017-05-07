14abe1757238628ff50fe59857bafb641f384757
Tzu-ping Chung <uranusjr@gmail.com>
2017-05-08 01:24:59 +0800

    Allow empty custom event title in admin

--- src/events/forms.py
+++ src/events/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 
-from .models import Schedule
+from .models import CustomEvent, Schedule
 
 
 class ScheduleCreationForm(forms.ModelForm):
     class Meta:
         model = Schedule
         fields = ['html']
+
+
+class CustomEventForm(forms.ModelForm):
+
+    class Meta:
+        model = CustomEvent
+        fields = [
+            'conference', 'title', 'begin_time', 'end_time', 'location',
+            'break_event',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].strip = False
