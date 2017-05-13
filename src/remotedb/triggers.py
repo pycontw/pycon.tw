38803b63d4a82a612b2cce063f267713f18005f3
Tzu-ping Chung <uranusjr@gmail.com>
2017-05-13 18:15:00 +0800

    Local functions are not picklable (sadface)

--- src/remotedb/triggers.py
+++ src/remotedb/triggers.py
@@ -39,75 +39,75 @@ def update_firebase(key, data):
     app.put(settings.FIREBASE_DB, key, data)
 
 
+def _sync_proposal_detail(proposal):
+    data = {str(proposal.pk): dumpers.dump_proposal(proposal)}
+    update_firebase('events', data)
+
+
 @mock_on_debug
 def sync_proposal_detail(proposal):
+    tasks.async(_sync_proposal_detail, proposal)
 
-    def f(proposal):
-        data = {str(proposal.pk): dumpers.dump_proposal(proposal)}
-        update_firebase('events', data)
 
-    tasks.async(f, proposal)
+def _sync_sponsored_talk_detail(event):
+    detail_data = dumpers.dump_sponsored_event_detail(event)
+    data = {'sponsored_{}'.format(event.pk): detail_data}
+    update_firebase('events', data)
 
 
 @mock_on_debug
 def sync_sponsored_talk_detail(event):
+    tasks.async(_sync_sponsored_talk_detail, event)
 
-    def f(event):
-        detail_data = dumpers.dump_sponsored_event_detail(event)
-        data = {'sponsored_{}'.format(event.pk): detail_data}
-        update_firebase('events', data)
 
-    tasks.async(f, event)
+def _sync_schedule():
+    custeom_event_qs = (
+        CustomEvent.objects
+        .select_related('begin_time', 'end_time')
+    )
+    keynote_event_qs = (
+        KeynoteEvent.objects
+        .select_related('begin_time', 'end_time')
+    )
+    proposed_talk_event_qs = (
+        ProposedTalkEvent.objects
+        .select_related('begin_time', 'end_time', 'proposal__submitter')
+    )
+    sponsored_event_qs = (
+        SponsoredEvent.objects
+        .select_related('begin_time', 'end_time', 'host')
+    )
+    schedule_data = dumpers.dump_schedule(itertools.chain(
+        custeom_event_qs,
+        keynote_event_qs,
+        proposed_talk_event_qs,
+        sponsored_event_qs,
+    ))
+    data = {
+        key: {'date': key, 'slots': value}
+        for key, value in schedule_data.items()
+    }
+    update_firebase('schedule', data)
 
 
 @mock_on_debug
 def sync_schedule():
+    tasks.async(_sync_schedule)
 
-    def f():
-        custeom_event_qs = (
-            CustomEvent.objects
-            .select_related('begin_time', 'end_time')
-        )
-        keynote_event_qs = (
-            KeynoteEvent.objects
-            .select_related('begin_time', 'end_time')
-        )
-        proposed_talk_event_qs = (
-            ProposedTalkEvent.objects
-            .select_related('begin_time', 'end_time', 'proposal__submitter')
-        )
-        sponsored_event_qs = (
-            SponsoredEvent.objects
-            .select_related('begin_time', 'end_time', 'host')
-        )
-        schedule_data = dumpers.dump_schedule(itertools.chain(
-            custeom_event_qs,
-            keynote_event_qs,
-            proposed_talk_event_qs,
-            sponsored_event_qs,
-        ))
-        data = {
-            key: {'date': key, 'slots': value}
-            for key, value in schedule_data.items()
-        }
-        update_firebase('schedule', data)
 
-    tasks.async(f)
+def _sync_user_events(user):
+    data = {
+        'sponsored_{}'.format(e.pk): dumpers.dump_sponsored_event_detail(e)
+        for e in SponsoredEvent.objects.filter(host=user)
+    }
+    data.update({
+        str(p.pk): dumpers.dump_proposal(p)
+        for p in TalkProposal.objects.filter_viewable(user=user)
+    })
+    if data:
+        update_firebase('events', data)
 
 
 @mock_on_debug
 def sync_user_events(user):
-
-    def f(user):
-        data = {
-            'sponsored_{}'.format(e.pk): dumpers.dump_sponsored_event_detail(e)
-            for e in SponsoredEvent.objects.filter(host=user)
-        }
-        data.update({
-            str(p.pk): dumpers.dump_proposal(p)
-            for p in TalkProposal.objects.filter_viewable(user=user)
-        })
-        if data:
-            update_firebase('events', data)
-
-    tasks.async(f)
+    tasks.async(_sync_user_events, user)
