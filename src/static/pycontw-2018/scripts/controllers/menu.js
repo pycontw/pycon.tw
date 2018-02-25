cd9f682d382da371da0e63e99f23f651744c8c21
Tzu-ping Chung <uranusjr@gmail.com>
2018-02-25 19:01:41 +0800

    Sync styling

--- src/static/pycontw-2018/scripts/controllers/menu.js
+++ src/static/pycontw-2018/scripts/controllers/menu.js
@@ -9,8 +9,10 @@ export class MenuController extends Controller {
 		document.documentElement.addEventListener('click', event => {
 			// Don't run if the event originates from the menu, to avoid triggering
 			// the handler twice when clicking on a menu item. Otherwise a menu item
-			// will not be able to uncheck itself.
-			if (!self.itemTargets.includes(event.target)) {
+			// will not be able to uncheck itself. Also notice we intentionally
+			// exclude label elements to avoid it triggering the input's event.
+			if (event.target.tagName !== 'LABEL' &&
+					!this.itemTargets.includes(event.target)) {
 				this.exclude(event)
 			}
 		})
