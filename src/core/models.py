a00b13ffeae52f2077453ff918096961c929a860
jneo8 <james0910238727@gmail.com>
2018-03-04 07:28:29 +0000

    Align

--- src/core/models.py
+++ src/core/models.py
@@ -93,7 +93,7 @@ class EventInfo(models.Model):
         ('OTHER', _('Other')),
         ('CORE',  _('Python Core (language, stdlib, etc.)')),
         ('INTNL', _('Python Internals')),
-        ('IOT', _('The Internet of things')),
+        ('IOT',   _('Internet Of Things')),
         ('LIBS',  _('Python Libraries')),
         ('SCI',   _('Science')),
         ('SEC',   _('Security')),
