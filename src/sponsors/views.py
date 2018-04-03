7b47be3829d10d0873a4543bd75d27f920f5a114
Tzu-ping Chung <uranusjr@gmail.com>
2018-04-03 13:42:18 +0800

    Remove dedicated sponsor page

--- src/sponsors/views.py
+++ src/sponsors/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import ListView
 
+from core.utils import TemplateExistanceStatusResponse
+
 from .models import Sponsor
 
 
 class SponsorListView(ListView):
 
     model = Sponsor
+    response_class = TemplateExistanceStatusResponse
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
