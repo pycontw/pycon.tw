f35361e9a985ec33bdb1ba4c8d8b647016137dba
Tzu-ping Chung <uranusjr@gmail.com>
2018-04-21 02:09:36 +0800

    Access Thunderforest tiles via HTTPS

--- src/static/pycontw-2018/scripts/venue.js
+++ src/static/pycontw-2018/scripts/venue.js
@@ -22,7 +22,7 @@ L.marker([25.040997, 121.611417], {
 const mbAttr1 = 'Tiles &copy; <a href="https://stamen.com" target="_blank" rel="noopener">Stamen Design</a>, and <a href="https://www.mapbox.com/" target="_blank" rel="noopener">Mapbox</a>. Data &copy; <a href="https://openstreetmap.org" target="_blank" rel="noopener">OpenStreetMap</a> contributors.'
 const mbUrl1 = 'http://{s}.sm.mapstack.stamen.com/((toner-background,$fff[@20],$002660[hsl-color]),(toner-lines,$fff[@10],$3b097b[hsl-color])[@90],(buildings,$fff[@20],$002660[hsl-color])[@20],(toner-labels,$fff[@40],$b2b1dc[hsl-color]))/{z}/{x}/{y}.png'
 const mbAttr2 = 'Maps &copy; <a href="https://www.thunderforest.com" target="_blank" rel="noopener">Thunderforest</a>, Data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap </a> contributors.'
-const mbUrl2 = 'http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=6170aad10dfd42a38d4d8c709a536f38'
+const mbUrl2 = 'https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=6170aad10dfd42a38d4d8c709a536f38'
 
 
 const stamen = L.tileLayer(mbUrl1, {attribution: mbAttr1})
