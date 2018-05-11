0c4d58c439612a13498aa09acd53e0033971745a
Tzu-ping Chung <uranusjr@gmail.com>
2018-05-12 00:59:07 +0800

    Rewrite the schedule script
    Purge jQuery and clean up unneeded things

--- src/static/pycontw-2018/scripts/schedule.js
+++ src/static/pycontw-2018/scripts/schedule.js
@@ -1,38 +1,32 @@
-import $ from 'jquery'
-
-$(() => {
-
 // Schedule generation.
-const $form = $('.generation-form')
-$form.css('display', 'block')
-$form.find('input[name="html"]').val($('.schedule-content').html())
+const form = document.querySelector('.generation-form')
+if (form) {
+	const html = document.querySelector('.schedule-content').innerHTML
+	form.querySelector('input[name="html"]').value = html
+	form.style.display = 'block'
+}
 
 
 // Replace localed URL with current locale prefix.
-window.I18N = JSON.parse($('#i18n_variables').text())
+const I18N = JSON.parse(document.getElementById('i18n_variables').textContent)
 function findPrefix(u) {
 	const possiblePrefixes = []
-	for (const prefix of window.I18N.LANGUAGE_PREFIXES) {
+	for (const prefix of I18N.LANGUAGE_PREFIXES) {
 		if (u.startsWith(prefix)) {
 			return prefix
 		}
 	}
 	return ''
 }
-$('.localed-url').each($e => {
-	const original = $e.attr('href')
+for (const el of document.querySelectorAll('.localed-url')) {
+	const original = el.getAttribute('href')
 	if (!original) {
-		return
+		continue
 	}
 	const prefix = findPrefix(original)
 	if (!prefix) {
-		return
+		continue
 	}
 	const sub = original.substr(prefix.length)
-	const replaced = `${window.I18N.LANGUAGE_PREFIX}${sub}`
-	if (replaced !== original) {
-		$e.attr('href', replaced)
-	}
-})
-
-})
+	el.setAttribute('href', `${I18N.LANGUAGE_PREFIX}${sub}`)
+}
