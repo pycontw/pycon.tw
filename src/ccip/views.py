83345b047c745fce6056a416564291480ad9101e
jneo8 <james0910238727@gmail.com>
2018-05-16 16:04:37 +0000

    New app ccip & copy ccip api in ccip app.

---
+++ src/ccip/views.py
@@ -0,0 +1,40 @@
+import json
+from django.http import JsonResponse
+from django.views.generic import View
+from django.contrib.staticfiles import finders
+
+from events.models import ProposedTalkEvent
+from events.views import transform_keynote_info
+
+# Create your views here.
+
+class CCIPAPIView(View):
+
+    def get(self, request):
+        dataset = [
+            {
+                'id': f'talk-{event.pk}',
+                'subject': event.proposal.title,
+                'summary': event.proposal.abstract,
+                'type': '',     # Not used.
+                'room': event.get_location_display(),
+                'start': event.begin_time.value.isoformat(),
+                'end': event.end_time.value.isoformat(),
+                'speaker': {
+                    'name': event.proposal.submitter.speaker_name,
+                    'avatar': request.build_absolute_uri(
+                        event.proposal.submitter.get_photo_url(),
+                    ),
+                    'bio': event.proposal.submitter.bio,
+                },
+            }
+            for event in ProposedTalkEvent.objects.select_related(
+                'proposal', 'proposal__submitter',
+            )
+        ]
+        with open(finders.find('pycontw-2018/assets/keynotes/info.json')) as f:
+            dataset.extend(
+                transform_keynote_info(request, i, info)
+                for i, info in enumerate(json.load(f), 1)
+            )
+        return JsonResponse(dataset, safe=False)
