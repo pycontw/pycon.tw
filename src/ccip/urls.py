dae940cc4a2b02f1691713cefd175f18e48c6ffb
jneo8 <james0910238727@gmail.com>
2018-05-18 12:36:08 +0000

    Add ccip/staff page

--- src/ccip/urls.py
+++ src/ccip/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # API for CCIP
     url(r'^$', views.CCIPAPIView.as_view()),
     url(r'^sponsors/$', views.CCIPSponsorsView.as_view()),
+    url(r'^staff/$', views.CCIPStaffView.as_view()),
 ]
