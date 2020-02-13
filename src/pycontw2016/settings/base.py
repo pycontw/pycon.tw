197a6c02ad3c789dd44954c598caa5d629c43cd2
MadRex2000 <spplkk113@gmail.com>
2020-02-13 18:21:07 +0800

    update src/pycontw2016/settings/base.py

--- src/pycontw2016/settings/base.py
+++ src/pycontw2016/settings/base.py
@@ -325,8 +325,8 @@ REVIEWS_VISIBLE_TO_SUBMITTERS = True
 
 TALK_PROPOSAL_DURATION_CHOICES = (
     ('NOPREF', _('No preference')),
-    ('PREF30', _('Prefer 30min')),
-    ('PREF45', _('Prefer 45min')),
+    ('PREF30', _('Prefer 15min')),
+    ('PREF45', _('Prefer 30min')),
 )
 
 EVENTS_DAY_NAMES = collections.OrderedDict([
