03aa2e65aa7b07c8b8982a0b6764e8793dfa66c3
Tom Chen <ctchen@gmail.com>
2020-02-16 22:05:26 +0800

    Add both main and tutorial venue and maps

--- src/static/pycontw-2020/scripts/venue.js
+++ src/static/pycontw-2020/scripts/venue.js
@@ -2,22 +2,23 @@ import L from 'leaflet'
 import 'leaflet-easybutton'
 
 const venue = L.layerGroup()
-const coordinates = [22.997908, 120.215657] // 成大國際會議廳
 
 // Marker style and layer definition.
-L.marker(coordinates, {
-	icon: L.icon({
-		iconUrl: window.VENUE_ICON,
-		iconSize: [100, 78],
-		iconAnchor: [50, 55],
-	}),
-})
-.addTo(venue)
-.bindTooltip(window.VENUE_NAME, {
-	offset: [-4, 20],
-	direction: 'bottom',
-})
-.openTooltip()
+window.MARKERS.forEach(function(entry) {
+    L.marker(entry.coord, {
+        icon: L.icon({
+            iconUrl: window.VENUE_ICON,
+            iconSize: [100, 78],
+            iconAnchor: [50, 55],
+        }),
+    })
+    .addTo(venue)
+    .bindTooltip(entry.name, {
+        offset: [-4, 20],
+        direction: 'bottom',
+    })
+    .openTooltip()
+});
 
 // Tile attributions.
 const mbAttr1 = 'Tiles by <a href="https://stamen.com">Stamen Design</a>. Data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors.'
@@ -31,8 +32,8 @@ const transport = L.tileLayer(mbUrl2, {attribution: mbAttr2})
 
 // Initialize map.
 const pymap = L.map('venue-map', {
-	center: coordinates,
-	zoom: 12,
+	center: window.MARKERS[0].coord,
+	zoom: 15,
 	layers: [stamen, venue],
 	scrollWheelZoom: false,
 	zoomControl: false,
@@ -44,12 +45,12 @@ function centerMap() {
 	const mapw = document.getElementById('venue-map').clientWidth
 	const ovlw = document.getElementById('venue-info-overlay').clientWidth
 
-	let lng = coordinates[1]
+	let lng = window.MARKERS[0].coord[1]
 	if (ovlw && mapw > ovlw) {
 		const bs = pymap.getBounds()
 		lng -= (bs.getEast() - bs.getWest()) * ovlw / mapw / 2
 	}
-	pymap.panTo([coordinates[0], lng])
+	pymap.panTo([window.MARKERS[0].coord[0], lng])
 }
 centerMap()
 window.map = pymap
