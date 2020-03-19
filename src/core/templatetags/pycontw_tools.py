1b883b1f8fec13f235bc714a17227fb56d83f288
Matt Wang <mattwang44@gmail.com>
2020-03-20 05:51:46 +0800

    Get path category with customized template filter

--- src/core/templatetags/pycontw_tools.py
+++ src/core/templatetags/pycontw_tools.py
@@ -1,3 +1,5 @@
+import re
+
 from django.template import Library
 
 
@@ -7,3 +9,22 @@ register = Library()
 @register.filter
 def message_bootstrap_class_str(message):
     return ' '.join('alert-' + tag for tag in message.tags.split(' '))
+
+
+@register.filter
+def get_path_category(url):
+    lang = '\/(zh\-hant|en\-us)'
+    category_pattern_mapping = {
+        'about': '\/about/pycontw',
+        'sponsor': '\/sponsor/sponsor',
+        'speaking': '\/speaking\/(cfp|talk|tutorial|recording)',
+        'conference': '\/(events\/(overview|schedule)|portal)',
+        'event': '\/events\/(keynotes|talks|open-spaces)',
+        'registration': '\/registration/(financial-aid|ticket-info|registration)',
+        'venue': '\/venue'
+    }
+    end = '\/?$'
+    for category, pattern in category_pattern_mapping.items():
+        if re.match(lang + pattern + end, url):
+            return category
+    return 'uncategorized'
