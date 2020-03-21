a03e412b3376dc795117e27ca1a17b24ba8c782b
Matt Wang <mattwang44@gmail.com>
2020-03-21 14:53:27 +0800

    Remove interdependence from get_path_category()

--- src/core/templatetags/pycontw_tools.py
+++ src/core/templatetags/pycontw_tools.py
@@ -13,18 +13,8 @@ def message_bootstrap_class_str(message):
 
 @register.filter
 def get_path_category(url):
-    lang = '\/(zh\-hant|en\-us)'
-    category_pattern_mapping = {
-        'about': '\/about/pycontw',
-        'sponsor': '\/sponsor/sponsor',
-        'speaking': '\/speaking\/(cfp|talk|tutorial|recording)',
-        'conference': '\/(events\/(overview|schedule)|portal)',
-        'event': '\/events\/(keynotes|talks|open-spaces)',
-        'registration': '\/registration/(financial-aid|ticket-info|registration)',
-        'venue': '\/venue'
-    }
-    end = '\/?$'
-    for category, pattern in category_pattern_mapping.items():
-        if re.match(lang + pattern + end, url):
-            return category
-    return 'uncategorized'
+    pattern = r'/(?P<lang>zh\-hant|en\-us)/(?P<category>[0-9a-z-]*)/'
+    result = re.match(pattern, url)
+    if not result:
+        return 'unmatched'
+    return result.groupdict().get('category', 'uncategorized')
