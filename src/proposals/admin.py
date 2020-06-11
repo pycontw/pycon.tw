c11ba5732ff1e679b1388d6c8d65816ec5dea8a3
Maliao Guo <MaliaoTW@gmail.com>
2020-06-11 19:37:54 +0800

    update TalkProposal export columns

--- src/proposals/admin.py
+++ src/proposals/admin.py
@@ -5,7 +5,7 @@ from import_export.admin import ExportMixin
 
 from .models import AdditionalSpeaker, TalkProposal, TutorialProposal
 from .resources import TalkProposalResource
-
+from reviews.models import Review
 
 class AdditionalSpeakerInline(GenericTabularInline):
     model = AdditionalSpeaker
@@ -17,12 +17,37 @@ class AdditionalSpeakerInline(GenericTabularInline):
 
 class ProposalAdmin(admin.ModelAdmin):
 
-    def vote(self,obj):
-        return sum([int(review.vote) for review in obj.review_set.all()])
+    def stage_1_plus_1_count(self,obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ONE).count()
+
+    def stage_1_plus_0_count(self,obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ZERO).count()
+
+    def stage_1_minus_0_count(self,obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ZERO).count()
+
+    def stage_1_minus_1_count(self,obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ONE).count()
+
+    def stage_2_plus_1_count(self,obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ONE).count()
+
+    def stage_2_plus_0_count(self,obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ZERO).count()
+
+    def stage_2_minus_0_count(self,obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ZERO).count()
+
+    def stage_2_minus_1_count(self,obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ONE).count()
+
+
 
     list_display = [
         'title', 'category', 'duration', 'language',
-        'python_level', 'remoting_policy', 'cancelled','accepted','vote'
+        'python_level', 'remoting_policy', 'cancelled','accepted',
+        'stage_1_plus_1_count','stage_1_plus_0_count','stage_1_minus_0_count','stage_1_minus_1_count',
+        'stage_2_plus_1_count','stage_2_plus_0_count','stage_2_minus_0_count','stage_2_minus_1_count'
     ]
     list_filter = [
         'cancelled', 'accepted',
--- src/proposals/resources.py
+++ src/proposals/resources.py
@@ -1,21 +1,51 @@
 from import_export import fields, resources
 
 from .models import TalkProposal
-
+from reviews.models import Review
 
 class TalkProposalResource(resources.ModelResource):
 
     name = fields.Field(attribute='submitter__speaker_name')
     email = fields.Field(attribute='submitter__email')
-    vote = fields.Field()
+    stage_1_plus_1_count = fields.Field()
+    stage_1_plus_0_count = fields.Field()
+    stage_1_minus_0_count = fields.Field()
+    stage_1_minus_1_count = fields.Field()
+    stage_2_plus_1_count = fields.Field()
+    stage_2_plus_0_count = fields.Field()
+    stage_2_minus_0_count = fields.Field()
+    stage_2_minus_1_count = fields.Field()
 
-    def dehydrate_vote(self, obj):
-        return sum([int(review.vote) for review in obj.review_set.all()])
+    def dehydrate_stage_1_plus_1_count(self, obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ONE).count()
+
+    def dehydrate_stage_1_plus_0_count(self, obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ZERO).count()
+
+    def dehydrate_stage_1_minus_0_count(self, obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ZERO).count()
+
+    def dehydrate_stage_1_minus_1_count(self, obj):
+        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ONE).count()
+
+    def dehydrate_stage_2_plus_1_count(self, obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ONE).count()
+
+    def dehydrate_stage_2_plus_0_count(self, obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ZERO).count()
+
+    def dehydrate_stage_2_minus_0_count(self, obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ZERO).count()
+
+    def dehydrate_stage_2_minus_1_count(self, obj):
+        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ONE).count()
 
     class Meta:
         model = TalkProposal
         fields = [
             'id', 'title', 'category', 'python_level', 'duration',
-            'language', 'name', 'email','cancelled','accepted','vote'
+            'language', 'name', 'email','cancelled','accepted',
+            'stage_1_plus_1_count', 'stage_1_plus_0_count', 'stage_1_minus_0_count', 'stage_1_minus_1_count',
+            'stage_2_plus_1_count', 'stage_2_plus_0_count', 'stage_2_minus_0_count', 'stage_2_minus_1_count'
         ]
         export_order = fields
