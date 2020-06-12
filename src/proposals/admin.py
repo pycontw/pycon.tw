ec62ae5de95c38c6c4740b2d1190c46f0a51614b
Maliao Guo <MaliaoTW@gmail.com>
2020-06-12 19:24:25 +0800

    remove unwanted TalkProposal export columns

--- src/proposals/admin.py
+++ src/proposals/admin.py
@@ -5,7 +5,7 @@ from import_export.admin import ExportMixin
 
 from .models import AdditionalSpeaker, TalkProposal, TutorialProposal
 from .resources import TalkProposalResource
-from reviews.models import Review
+
 
 class AdditionalSpeakerInline(GenericTabularInline):
     model = AdditionalSpeaker
@@ -16,38 +16,10 @@ class AdditionalSpeakerInline(GenericTabularInline):
 
 
 class ProposalAdmin(admin.ModelAdmin):
-
-    def stage_1_plus_1_count(self,obj):
-        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ONE).count()
-
-    def stage_1_plus_0_count(self,obj):
-        return obj.review_set.filter(stage=1,vote=Review.Vote.PLUS_ZERO).count()
-
-    def stage_1_minus_0_count(self,obj):
-        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ZERO).count()
-
-    def stage_1_minus_1_count(self,obj):
-        return obj.review_set.filter(stage=1,vote=Review.Vote.MINUS_ONE).count()
-
-    def stage_2_plus_1_count(self,obj):
-        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ONE).count()
-
-    def stage_2_plus_0_count(self,obj):
-        return obj.review_set.filter(stage=2,vote=Review.Vote.PLUS_ZERO).count()
-
-    def stage_2_minus_0_count(self,obj):
-        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ZERO).count()
-
-    def stage_2_minus_1_count(self,obj):
-        return obj.review_set.filter(stage=2,vote=Review.Vote.MINUS_ONE).count()
-
-
-
     list_display = [
         'title', 'category', 'duration', 'language',
-        'python_level', 'remoting_policy', 'cancelled','accepted',
-        'stage_1_plus_1_count','stage_1_plus_0_count','stage_1_minus_0_count','stage_1_minus_1_count',
-        'stage_2_plus_1_count','stage_2_plus_0_count','stage_2_minus_0_count','stage_2_minus_1_count'
+        'python_level', 'accepted',
+        'python_level', 'remoting_policy', 'accepted',
     ]
     list_filter = [
         'cancelled', 'accepted',
@@ -57,6 +29,7 @@ class ProposalAdmin(admin.ModelAdmin):
         'conference', 'submitter', 'title', 'category', 'duration',
         'language', 'abstract', 'python_level', 'objective',
         'detailed_description', 'outline', 'supplementary',
+        'recording_policy', 'slide_link', 'referring_policy', 'cancelled',
         'recording_policy', 'slide_link', 'referring_policy', 'remoting_policy', 'cancelled',
         'accepted', 'slido_embed_link',
     ]
@@ -73,3 +46,4 @@ class TalkProposalAdmin(ExportMixin, ProposalAdmin):
 @admin.register(TutorialProposal)
 class TutorialProposalAdmin(ProposalAdmin):
     pass
+
