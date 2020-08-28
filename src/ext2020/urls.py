ffee742b81df38b16690a2b26a8ffd6a93d376ce
Tom Chen <ctchen@gmail.com>
2020-08-29 00:50:40 +0800

    Fix ext2020 trailing slash problem

--- src/ext2020/urls.py
+++ src/ext2020/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^live$', views.live, name='live'),
-    url(r'^discord$', views.discord, name='discord'),
+    url(r'^live/$', views.live, name='live'),
+    url(r'^discord/$', views.discord, name='discord'),
 ]
