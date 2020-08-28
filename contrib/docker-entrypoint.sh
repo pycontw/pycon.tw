5fb79db855864a4f420156da391b68c6dbdf71e8
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2020-08-28 10:16:50 +0800

    test(db): inject DB testing data
    issue #891

--- contrib/docker-entrypoint.sh
+++ contrib/docker-entrypoint.sh
@@ -3,6 +3,8 @@ echo 'Run migration'
 python3 /app/src/manage.py migrate
 echo 'Create super user'
 python3 /app/src/manage.py createsuperuser --noinput || echo "Super user already created"
+echo 'Inject DB testing data'
+python3 /app/src/manage.py loaddata /db-testing-data.json --exclude=users.user
 echo 'Compile localized translation'
 python3 /app/src/manage.py compilemessages
 exec "$@"
