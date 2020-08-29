2a36ce3fe31cf93aac91a910705691e8bab25fc0
Tom Chen <ctchen@gmail.com>
2020-08-29 14:50:28 +0800

    Refactor ext views from FBV to CBV, and add headless endpoints for OPass

--- src/ext2020/urls.py
+++ src/ext2020/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^live/$', views.live, name='live'),
-    url(r'^discord/$', views.discord, name='discord'),
+    url(r'^live/$', views.LiveBroadcastView.as_view(), name='live'),
+    url(r'^discord/$', views.DiscordView.as_view(), name='discord'),
 ]
