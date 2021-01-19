5a95e2a4b09169d171d429e1dcb69730aeda149b
Matt.Wang <mattwang44@gmail.com>
2021-01-20 01:42:25 +0800

    chore(setting): add 2021 setting

---
+++ src/pycontw2016/settings/testing/pycontw2021.py
@@ -0,0 +1,13 @@
+import os
+
+from .base import BASE_DIR, STATICFILES_DIRS, TEMPLATES
+from .base import *     # noqa
+
+
+CONFERENCE_DEFAULT_SLUG = 'pycontw-2021'
+TEMPLATES[0]['DIRS'][1] = os.path.join(BASE_DIR, 'templates', 'pycontw-2021')
+STATICFILES_DIRS[1] = os.path.join(
+    BASE_DIR, 'static', CONFERENCE_DEFAULT_SLUG, '_includes',
+)
+
+EVENTS_PUBLISHED = False
