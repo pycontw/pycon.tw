11320d0b2d997189af170282b57652caf9c7e330
Matt.Wang <mattwang44@gmail.com>
2021-02-03 03:19:46 +0800

    fix(sponsor): remove modeltrans breaks DB consistency

--- src/sponsors/translation.py
+++ src/sponsors/translation.py
@@ -3,14 +3,14 @@ from modeltranslation.translator import translator, TranslationOptions
 from .models import Sponsor, OpenRole
 
 
-# class SponsorTranslationOptions(TranslationOptions):
-#     fields = ('intro',)
+class SponsorTranslationOptions(TranslationOptions):
+    fields = ('intro',)
 
 
-# class OpenRoleTranslationOptions(TranslationOptions):
-#     fields = ('description',)
+class OpenRoleTranslationOptions(TranslationOptions):
+    fields = ('description',)
 
 
-# translator.register(Sponsor, SponsorTranslationOptions)
+translator.register(Sponsor, SponsorTranslationOptions)
 
-# translator.register(OpenRole, OpenRoleTranslationOptions)
+translator.register(OpenRole, OpenRoleTranslationOptions)
