ecc99b3fff978b912ff69b1689f0fb429291c425
張櫻瀞 <alice6373@gmail.com>
2021-02-05 21:17:03 +0800

    Clear Prefer 45min in setting/base.py

--- src/pycontw2016/settings/base.py
+++ src/pycontw2016/settings/base.py
@@ -318,7 +318,6 @@ TALK_PROPOSAL_DURATION_CHOICES = (
     ('NOPREF', _('No preference')),
     ('PREF15', _('Prefer 15min')),
     ('PREF30', _('Prefer 30min')),
-    ('PREF45', _('Prefer 45min')),
 )
 
 EVENTS_DAY_NAMES = collections.OrderedDict([
