52c2304ec053c559b51da1c161379a5f1ef58261
Max_Home <a0025071@gmail.com>
2021-04-11 00:22:52 +0800

    feat(sponsors): remove the old sponsors api

--- src/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from sponsors.views import SponsorAPIView, JobAPIView
-
-urlpatterns = [
-    url(r'^sponsors/$', SponsorAPIView.as_view()),
-    url(r'^jobs/$', JobAPIView.as_view()),
-]
