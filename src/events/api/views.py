284c694346db2ced1a2e8919b7f73a4d0f8efaf6
Benson <bc842017@gmail.com>
2021-04-30 13:15:24 +0800

    add tutorialList api

--- src/events/api/views.py
+++ src/events/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import RetrieveAPIView
-
+from rest_framework import views
+from rest_framework.response import Response
+from proposals.models import TutorialProposal
 from events import models
 from . import serializers
 
@@ -8,3 +10,18 @@ class TalkDetailAPIView(RetrieveAPIView):
 
     queryset = models.TalkProposal.objects.all()
     serializer_class = serializers.TalkDetailSerializer
+
+class TutorialListAPIView(views.APIView):
+    def get(self, request):
+        tutorial_data = TutorialProposal.objects.all()
+
+        response_data = {"tutorials": []}
+        for tutorial in tutorial_data:
+            response_data["tutorials"].append({
+                "title": tutorial.title,
+                "abstract": tutorial.abstract,
+                "slide_url": tutorial.slide_link,
+                "registration_link": "no registration link yet"
+            })
+
+        return Response(response_data)
