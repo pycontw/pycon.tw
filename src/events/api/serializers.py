8e1e2e76ce5f94763d05c34cec1b2c2c11f9e802
Ethan Lin <dabingnai@gmail.com>
2021-05-27 22:39:05 +0800

    Fixed serializer for TalkDetailAPI

--- src/events/api/serializers.py
+++ src/events/api/serializers.py
@@ -46,10 +46,51 @@ class TalkDetailSerializer(serializers.ModelSerializer):
 
 
 class TutorialDetailSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+    date = serializers.SerializerMethodField()
+    begin_time = serializers.SerializerMethodField()
+    end_time = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    language = serializers.SerializerMethodField()
+    python_level = serializers.SerializerMethodField()
+    abstract = serializers.SerializerMethodField()
+    detailed_description = serializers.SerializerMethodField()
+    slide_link = serializers.SerializerMethodField()
+    slido_embed_link = serializers.SerializerMethodField()
     speakers = serializers.SerializerMethodField()
-    begin_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    end_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    date = begin_time
+
+    def get_title(self, obj):
+        return obj.proposal.title
+
+    def get_date(self, obj):
+        return obj.begin_time.value.strftime("%Y-%m-%d")
+
+    def get_begin_time(self, obj):
+        return obj.begin_time.value.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_end_time(self, obj):
+        return obj.end_time.value.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_category(self, obj):
+        return obj.proposal.category
+
+    def get_language(self, obj):
+        return obj.proposal.language
+
+    def get_python_level(self, obj):
+        return obj.proposal.python_level
+
+    def get_abstract(self, obj):
+        return obj.proposal.abstract
+
+    def get_detailed_description(self, obj):
+        return obj.proposal.detailed_description
+
+    def get_slide_link(self, obj):
+        return obj.proposal.slide_link
+
+    def get_slido_embed_link(self, obj):
+        return obj.proposal.slido_embed_link
 
     def get_speakers(self, obj):
         return [
@@ -59,7 +100,7 @@ class TutorialDetailSerializer(serializers.ModelSerializer):
                       'github_profile_url': i.user.github_profile_url,
                       'twitter_profile_url': i.user.twitter_profile_url,
                       'facebook_profile_url': i.user.facebook_profile_url}).get_initial(),
-                serializer=PrimarySpeakerSerializer) for i in obj.speakers]
+                serializer=PrimarySpeakerSerializer) for i in obj.proposal.speakers]
 
     class Meta:
         model = models.ProposedTutorialEvent
