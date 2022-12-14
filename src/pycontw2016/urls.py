4ef0894f3d44174a2e973341e3890cfce9962b1d
Wei-Hsiang (Matt) Wang <mattwang44@gmail.com>
2022-12-14 14:16:39 +0800

    fix: indent

--- src/pycontw2016/urls.py
+++ src/pycontw2016/urls.py
@@ -13,16 +13,16 @@ from users.views import user_dashboard
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
