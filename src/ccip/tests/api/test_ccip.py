c686cb283386aeb51fa4c04d7033c43d2f21a6ab
erik1110 <oceanuheart@gmail.com>
2023-09-16 11:40:24 +0800

    [feat] add rooms and session_types

--- src/ccip/tests/api/test_ccip.py
+++ src/ccip/tests/api/test_ccip.py
@@ -1,17 +1,82 @@
 import pytest
 from django.conf import settings
 
-endpoint = "/ccip"
+endpoint = "/ccip/"
+
 
 @pytest.mark.django_db
-def test_ccip_api_view(client, sample_tags):
-
+def test_(client):
     response = client.get(endpoint, follow=True)
     assert response.status_code == 200
-
     data = response.json()
+    assert 'rooms' in data
+    rooms = data['rooms']
+    for room in rooms:
+        assert 'id' in room
+        assert 'zh' in room
+        assert 'en' in room
 
-    assert 'tags' in data
-    assert isinstance(data['tags'], list)
-    print("data:", data)
-    assert data['tags'][0]['en']['name'] == "English talk"
+@pytest.mark.django_db
+def test_session_types(client):
+    response = client.get(endpoint, follow=True)
+    assert response.status_code == 200
+    expected_session_types = [
+        {
+            "id": "event",
+            "zh": {
+                "name": "活動"
+            },
+            "en": {
+                "name": "event"
+            }
+        },
+        {
+            "id": "break",
+            "zh": {
+                "name": "休息"
+            },
+            "en": {
+                "name": "break"
+            }
+        },
+        {
+            "id": "keynote",
+            "zh": {
+                "name": "基調演講"
+            },
+            "en": {
+                "name": "keynote"
+            }
+        },
+        {
+            "id": "sponsored",
+            "zh": {
+                "name": "贊助"
+            },
+            "en": {
+                "name": "sponsored"
+            }
+        },
+        {
+            "id": "tutorial",
+            "zh": {
+                "name": "專業課程"
+            },
+            "en": {
+                "name": "tutorial"
+            }
+        },
+        {
+            "id": "talk",
+            "zh": {
+                "name": "演講"
+            },
+            "en": {
+                "name": "talk"
+            }
+        }
+    ]
+    data = response.json()
+    assert 'session_types' in data
+    assert data['session_types'] == expected_session_types
+
