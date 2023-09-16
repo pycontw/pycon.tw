92dfdc5264e8a2516307aa83dcffa9c8ee292bae
erik1110 <oceanuheart@gmail.com>
2023-09-16 11:53:25 +0800

    [feat] add data structure test

--- src/ccip/tests/api/test_ccip.py
+++ src/ccip/tests/api/test_ccip.py
@@ -3,80 +3,21 @@ from django.conf import settings
 
 endpoint = "/ccip/"
 
+def assert_data_structure(data, key):
+    items = data.get(key, [])
+    for item in items:
+        assert 'id' in item
+        assert 'zh' in item
+        assert 'en' in item
+        assert 'name' in item['zh']
+        assert 'name' in item['en']
 
 @pytest.mark.django_db
-def test_(client):
+def test_data_structure(client):
     response = client.get(endpoint, follow=True)
     assert response.status_code == 200
     data = response.json()
-    assert 'rooms' in data
-    rooms = data['rooms']
-    for room in rooms:
-        assert 'id' in room
-        assert 'zh' in room
-        assert 'en' in room
 
-@pytest.mark.django_db
-def test_session_types(client):
-    response = client.get(endpoint, follow=True)
-    assert response.status_code == 200
-    expected_session_types = [
-        {
-            "id": "event",
-            "zh": {
-                "name": "活動"
-            },
-            "en": {
-                "name": "event"
-            }
-        },
-        {
-            "id": "break",
-            "zh": {
-                "name": "休息"
-            },
-            "en": {
-                "name": "break"
-            }
-        },
-        {
-            "id": "keynote",
-            "zh": {
-                "name": "基調演講"
-            },
-            "en": {
-                "name": "keynote"
-            }
-        },
-        {
-            "id": "sponsored",
-            "zh": {
-                "name": "贊助"
-            },
-            "en": {
-                "name": "sponsored"
-            }
-        },
-        {
-            "id": "tutorial",
-            "zh": {
-                "name": "專業課程"
-            },
-            "en": {
-                "name": "tutorial"
-            }
-        },
-        {
-            "id": "talk",
-            "zh": {
-                "name": "演講"
-            },
-            "en": {
-                "name": "talk"
-            }
-        }
-    ]
-    data = response.json()
-    assert 'session_types' in data
-    assert data['session_types'] == expected_session_types
-
+    assert_data_structure(data, 'session_types')
+    assert_data_structure(data, 'tags')
+    assert_data_structure(data, 'rooms')
