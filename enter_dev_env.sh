7341b7d130f5b3c0f0cbfae2ebeefd6714c2972c
Wei-Hsiang (Matt) Wang <mattwang44@gmail.com>
2024-04-18 14:52:15 +0000

    refactor(dev): update var name and adopt compose v2

--- enter_dev_env.sh
+++ enter_dev_env.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -xe
-CONTAINER="pycontw"
+CONTAINER="app-dev"
+PROJECT_NAME="pycontw_backend_dev"
 COMPOSE_FILE="./docker-compose-dev.yml"
+COMPOSE_CMD="docker compose -f $COMPOSE_FILE -p ${PROJECT_NAME}"
 
 # test if the container is running
-HASH=`docker ps -q -f name=$CONTAINER`
+HASH=`docker ps -q -f name="${PROJECT_NAME}-${CONTAINER}-1"`
 
 # test if the container is stopped
-HASH_STOPPED=`docker ps -qa -f name=$CONTAINER`
+HASH_STOPPED=`docker ps -qa -f name="${PROJECT_NAME}-${CONTAINER}-1"`
 
 if [[ $(uname -m) == 'arm64' ]]; then
   export DOCKER_DEFAULT_PLATFORM=linux/amd64
@@ -14,14 +16,14 @@ fi
 
 if [ -n "$HASH" ];then
     echo "found existing running container $CONTAINER, proceeding to exec another shell"
-    docker-compose -f $COMPOSE_FILE exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
+    $COMPOSE_CMD exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
 elif [ -n "$HASH_STOPPED" ];then
     echo "found existing stopped container $CONTAINER, starting"
-    (docker-compose -f $COMPOSE_FILE restart && docker start $HASH_STOPPED) >/dev/null 2>&1
-    docker-compose -f $COMPOSE_FILE exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
+    ($COMPOSE_CMD restart && docker start $HASH_STOPPED) >/dev/null 2>&1
+    $COMPOSE_CMD exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
 else
     echo "existing container not found, creating a new one, named $CONTAINER"
-    docker-compose -f $COMPOSE_FILE up --build --remove-orphans -d
-    docker-compose -f $COMPOSE_FILE exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
+    $COMPOSE_CMD up --build --remove-orphans -d
+    $COMPOSE_CMD exec -i $CONTAINER bash -c "SHELL=bash source /app/.venv/bin/activate && bash"
 fi
-echo "see you, use 'docker rm $CONTAINER' to kill the dev container or 'docker-compose -f $COMPOSE_FILE down' to kill both the postgres and the dev container if you want a fresh env next time"
+echo "see you, use '$COMPOSE_CMD down' to kill both the postgres and the dev container if you want a fresh env next time"
