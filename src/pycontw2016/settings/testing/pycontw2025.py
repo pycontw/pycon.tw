4a8e863c6f8322d920752f534101e3b84751e56c
Ritabear <zxc898977@gmail.com>
2025-01-07 20:19:11 +0800

    feat: add files to  PyConTW 2024 to 2025(BackEnd)

---
+++ src/pycontw2016/settings/testing/pycontw2025.py
@@ -0,0 +1,22 @@
+import os
+
+from django.utils.translation import gettext_lazy as _
+
+from .base import *  # noqa
+from .base import BASE_DIR, STATICFILES_DIRS, TEMPLATES
+
+CONFERENCE_DEFAULT_SLUG = 'pycontw-2025'
+
+TALK_PROPOSAL_DURATION_CHOICES = (
+    ('NOPREF', _('No preference')),
+    ('PREF15', _('Prefer 15min')),
+    ('PREF30', _('Prefer 30min')),
+    ('PREF45', _('Prefer 45min')),
+)
+
+TEMPLATES[0]['DIRS'][1] = os.path.join(BASE_DIR, 'templates', 'pycontw-2025')
+STATICFILES_DIRS[1] = os.path.join(
+    BASE_DIR, 'static', CONFERENCE_DEFAULT_SLUG, '_includes',
+)
+
+EVENTS_PUBLISHED = False
