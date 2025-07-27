36344d9215e2ef0c8f3324e53a594a8ea895757d
Rudolf Chen <rudd6617@gmail.com>
2025-07-27 12:24:13 +0800

    fix: sort reviewer

--- src/users/api/views.py
+++ src/users/api/views.py
@@ -8,22 +8,24 @@ from core.authentication import TokenAuthentication
 User = get_user_model()
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def user_list(request):
-    role = request.GET.get('role')
+    role = request.GET.get("role")
     if not role or role != "Reviewer":
-        return JsonResponse({'detail': 'role is not given or invalid.'}, status=400)
-    qs = User.objects.filter(is_active=True,verified=True, groups__name= "Reviewer")
+        return JsonResponse({"detail": "role is not given or invalid."}, status=400)
+    qs = User.objects.filter(is_active=True, verified=True, groups__name="Reviewer").order_by('speaker_name')
     users = []
     for user in qs:
-        users.append({
-            'full_name': user.get_full_name(),
-            'bio': user.bio,
-            'photo_url': user.get_thumbnail_url(default_value=None),
-            'facebook_profile_url': user.facebook_profile_url,
-            'twitter_profile_url': user.twitter_profile_url,
-            'github_profile_url': user.github_profile_url,
-        })
+        users.append(
+            {
+                "full_name": user.get_full_name(),
+                "bio": user.bio,
+                "photo_url": user.get_thumbnail_url(default_value=None),
+                "facebook_profile_url": user.facebook_profile_url,
+                "twitter_profile_url": user.twitter_profile_url,
+                "github_profile_url": user.github_profile_url,
+            }
+        )
     return JsonResponse(list(users), safe=False)
