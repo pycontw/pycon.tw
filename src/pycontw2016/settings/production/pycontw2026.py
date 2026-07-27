f34ddabe9c74d8e94e0004955cb36d0c32f6a05b
SerKo <serko.dev@gmail.com>
2026-07-27 14:14:10 +0800

    chore: update allowed hosts for Docker internal network (#1270)
    * chore: update allowed hosts for Docker internal network
    
    * fix: lint
    
    * refactor: seperate allow hosts container name variable
    
    Co-authored-by: W. H. Wang <mattwang44@gmail.com>
    
    * chore: simplify import from base in settings
    
    ---------
    
    Co-authored-by: W. H. Wang <mattwang44@gmail.com>

--- src/pycontw2016/settings/production/pycontw2026.py
+++ src/pycontw2016/settings/production/pycontw2026.py
@@ -3,7 +3,12 @@ import os
 from django.utils.translation import gettext_lazy as _
 
 from .base import *  # noqa
-from .base import BASE_DIR, STATICFILES_DIRS, TEMPLATES
+from .base import (
+    ALLOWED_HOSTS,
+    BASE_DIR,
+    STATICFILES_DIRS,
+    TEMPLATES,
+)
 
 # Override static and media URL for prefix in WSGI server.
 # https://code.djangoproject.com/ticket/25598
@@ -11,6 +16,10 @@ STATIC_URL = '/prs/static/'
 MEDIA_URL = '/prs/media/'
 
 CONFERENCE_DEFAULT_SLUG = 'pycontw-2026'
+
+# allow host for docker internal network (http://pycontw-202X[:XXXX])
+COMPOSE_SERVICE_CONTAINER_NAME = 'pycontw-2026'  # defined in docker-compose.yml
+ALLOWED_HOSTS = [*ALLOWED_HOSTS, COMPOSE_SERVICE_CONTAINER_NAME]
 
 TALK_PROPOSAL_DURATION_CHOICES = (
     ('NOPREF', _('No preference')),
